bfe889e24c771df6af3b69fd3683a1b203ea08a5
Kamruzzaman2200 <sohel.eightb@gmail.com>
2026-08-14 00:21:38 +0600

    chore: remove daisyui plugin and configuration from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+
 
 export default {
   content: [
@@ -54,9 +54,6 @@ export default {
       }
     },
   },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ["dark"],
-  },
+  plugins: [],
 }
 
